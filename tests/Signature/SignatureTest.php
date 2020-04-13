cf6f5122f5173e9568f75f3c2cc7a76f7ce08201
Sergey S. Smirnov <SergeySSmirnov@mail.ru>
2020-04-13 04:09:26 +0300

    Удалены старые файлы.
    Signed-off-by: Sergey S. Smirnov <SergeySSmirnov@mail.ru>

--- tests/Signature/SignatureTest.php
+++ tests/Signature/SignatureTest.php
@@ -12,7 +12,6 @@
 namespace Rusproj\Uniteller\Tests\Signature;
 
 use Rusproj\Uniteller\Signature\SignatureCallback;
-use Rusproj\Uniteller\Signature\SignatureRecurrent;
 use Rusproj\Uniteller\Tests\TestCase;
 use Rusproj\Uniteller\Signature\SignatureHandler;
 use Rusproj\Uniteller\Tests\Payment\FiscaliationPaymentBuilderTest;
@@ -91,19 +90,6 @@ class SignatureTest extends TestCase
         $this->assertTrue($_keys['IsRecurrentStart'] === '');
     }
 
-    public function testRecurrentSignatureCreation()
-    {
-        $sig = (new SignatureRecurrent())
-            ->setShopIdp('ACME')
-            ->setOrderIdp('FOO')
-            ->setSubtotalP(100)
-            ->setParentOrderIdp('BAR')
-            ->setPassword('LONG-PWD')
-            ->create();
-
-        $this->assertSame('A5FE1C95A2819EBACFC2145EE83742F6', $sig);
-    }
-
     public function testCallbackSignatureCreation()
     {
         $sig = (new SignatureCallback())
@@ -131,40 +117,4 @@ class SignatureTest extends TestCase
         $this->assertSame('1F4E3B63AE408D0BE1E33965E6697236', $sig);
     }
 
-    public function testRecurrentSignatureVerifying()
-    {
-        $sig = (new SignatureRecurrent())
-            ->setShopIdp('ACME')
-            ->setOrderIdp('FOO')
-            ->setSubtotalP(100)
-            ->setParentOrderIdp('BAR')
-            ->setPassword('LONG-PWD');
-
-        $this->assertTrue($sig->verify('A5FE1C95A2819EBACFC2145EE83742F6'));
-    }
-
-    public function testCallbackSignatureVerifying()
-    {
-        $sig = (new SignatureCallback())
-            ->setOrderId('FOO')
-            ->setStatus('paid')
-            ->setPassword('LONG-PWD');
-
-        $this->assertTrue($sig->verify('3F728AA479E50F5B10EE6C20258BFF88'));
-    }
-
-    public function testCallbackSignatureVerifyingWithFields()
-    {
-        $sig = (new SignatureCallback())
-            ->setOrderId('FOO')
-            ->setStatus('paid')
-            ->setFields([
-                'AcquirerID'   => 'fOO',
-                'ApprovalCode' => 'BaR',
-                'BillNumber'   => 'baz',
-            ])
-            ->setPassword('LONG-PWD');
-
-        $this->assertTrue($sig->verify('1F4E3B63AE408D0BE1E33965E6697236'));
-    }
 }
